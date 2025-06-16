5d1671e6e344d948b867a99d4ffa6ff4a0f94820
Prayag K <prayagk4@gmail.com>
2025-06-16 08:40:09 +0530

    Webpack bundle anaylzer configured

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import withBundleAnalyzer from "@next/bundle-analyzer";
 
 const ContentSecurityPolicy = `
   default-src 'self';
@@ -46,4 +47,6 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withBundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+})(nextConfig);
